e25145d2ffe69cd4d8c55e3e7a3d5f9fb4c65742
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-05-23 14:32:54 +0300

    [VYMCA-5] Authentication system: Base module and admin UI

---
+++ modules/openy_gc_auth/modules/openy_gc_auth_example/src/Plugin/GCIdentityProvider/Example.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\openy_gc_auth_example\Plugin\GCIdentityProvider;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\openy_gc_auth\GCIdentityProviderPluginBase;
+
+/**
+ * Example identity provider plugin.
+ *
+ * @GCIdentityProvider(
+ *   id="dummy",
+ *   label = @Translation("Dummy provider"),
+ *   config="openy_gc_auth.provider.dummy"
+ * )
+ */
+class Example extends GCIdentityProviderPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration():array {
+    return [
+      'user' => '',
+      'token' => '',
+      'password' => '',
+      'redirect_url' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['user'] = [
+      '#title' => $this->t('User'),
+      '#description' => $this->t('User name/email for access dummy identity provider.'),
+      '#type' => 'textfield',
+      '#default_value' => $config['user'],
+      '#required' => TRUE,
+    ];
+
+    $form['token'] = [
+      '#title' => $this->t('Api token'),
+      '#description' => $this->t('Some dummy API token, that will be used on frontend.'),
+      '#type' => 'textfield',
+      '#default_value' => $config['token'],
+      '#required' => TRUE,
+    ];
+
+    $form['password'] = [
+      '#title' => $this->t('Password'),
+      '#description' => $this->t('User password for access dummy identity provider. We recommend to not store this is configs, you can override this value in settings.php (config - openy_gc_auth.provider.example)'),
+      '#type' => 'password',
+      '#default_value' => $config['password'],
+      '#size' => 25,
+    ];
+
+    $form['redirect_url'] = [
+      '#title' => $this->t('Url'),
+      '#description' => $this->t('Identity provider url for redirect.'),
+      '#type' => 'textfield',
+      '#default_value' => $config['redirect_url'],
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    if (!$form_state->getErrors()) {
+      $this->configuration['user'] = $form_state->getValue('user');
+      $this->configuration['token'] = $form_state->getValue('token');
+      $this->configuration['redirect_url'] = $form_state->getValue('redirect_url');
+      if ($form_state->getValue('password')) {
+        // Override only in case empty value.
+        $this->configuration['password'] = $form_state->getValue('password');
+      }
+      parent::submitConfigurationForm($form, $form_state);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDataForApp():array {
+    $data = parent::getDataForApp();
+    $data['token'] = $this->configuration['token'];
+    $data['redirect_url'] = $this->configuration['redirect_url'];
+    return $data;
+  }
+
+}
---
+++ modules/openy_gc_auth/src/GCIdentityProviderPluginBase.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Drupal\openy_gc_auth;
+
+use Drupal\Component\Plugin\PluginBase;
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines the base plugin for GCIdentityProvider classes.
+ *
+ * @see \Drupal\openy_gc_auth\GCIdentityProviderManager
+ * @see \Drupal\openy_gc_auth\GCIdentityProviderInterface
+ * @see \Drupal\openy_gc_auth\Annotation\GCIdentityProvider
+ * @see plugin_api
+ */
+abstract class GCIdentityProviderPluginBase extends PluginBase implements GCIdentityProviderInterface, ContainerFactoryPluginInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config;
+    // We use pre-saved configuration here.
+    $configuration = $this->configFactory->get($this->getConfigName())->get();
+    $this->setConfiguration($configuration);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('config.factory'), $container->get('entity_type.manager'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getId() {
+    return $this->pluginDefinition['id'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabel() {
+    return $this->pluginDefinition['label'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfigName() {
+    return $this->pluginDefinition['config'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  private function baseConfigurationDefaults():array {
+    return [
+      // TODO: add here configs that can be similar for all instances.
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration():array {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfiguration() {
+    return $this->configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setConfiguration(array $configuration) {
+    $this->configuration = NestedArray::mergeDeep(
+      $this->baseConfigurationDefaults(),
+      $this->defaultConfiguration(),
+      $configuration
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    // $config = $this->getConfiguration();
+    $form['admin_label'] = [
+      '#type' => 'page_title',
+      '#title' => $this->getLabel(),
+    ];
+    // TODO: add here fields that can be similar for all instances.
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+    // This method not required.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    // Process the settings save if no errors occurred only.
+    if (!$form_state->getErrors()) {
+      // Save config in active storage.
+      $configuration = $this->configFactory->getEditable($this->getConfigName());
+      $configuration->setData($this->configuration);
+      $configuration->save();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDataForApp():array {
+    // $config = $this->configFactory->get($this->getConfigName());
+    return [
+      'type' => $this->getId(),
+    ];
+  }
+
+}
