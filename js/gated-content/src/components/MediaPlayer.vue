9155ecbee8cd491a42a572b436fcc5f539dc296f
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-06-11 12:48:36 +0300

    [VYMCA-36] Add support of Vimeo live streams

--- js/gated-content/src/components/MediaPlayer.vue
+++ js/gated-content/src/components/MediaPlayer.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <LazyYoutubeVideo ref="youtube-player" v-if="source === 'youtube'" :src="youtubeSrc"/>
-    <vueVimeoPlayer ref="vimeo-player" v-if="source === 'vimeo'" v-bind="vimeoOptions"/>
+    <vueVimeoPlayer
+      ref="vimeo-player"
+      v-if="source === 'vimeo' || source === 'vimeo_event'"
+      v-bind="vimeoOptions"
+    />
   </div>
 </template>
 
@@ -39,8 +43,10 @@ export default {
         },
       };
 
-      if (/https:\/\/vimeo\.com\/\d+\/[a-z0-9]+/i.test(this.media.field_media_video_embed_field)) {
-        // In case we have private video - set video-url option.
+      if (/https:\/\/vimeo\.com\/\d+\/[a-z0-9]+/i.test(this.media.field_media_video_embed_field)
+        || /https:\/\/vimeo\.com\/event\/\d+/i.test(this.media.field_media_video_embed_field)
+      ) {
+        // In case we have private video or event - set video-url option.
         // Example of private url - https://vimeo.com/426932693/cfbe98b981
         options['video-url'] = this.media.field_media_video_embed_field;
       }
