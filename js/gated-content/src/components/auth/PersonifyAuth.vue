dafae14ad607b7c842fb0c87b6d58199f3197a4e
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2020-07-31 10:38:18 +0300

    styles for Personify login error and try again button

--- js/gated-content/src/components/auth/PersonifyAuth.vue
+++ js/gated-content/src/components/auth/PersonifyAuth.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="plugin-custom personify-auth container">
-    <div v-if="this.error" class="alert alert-danger">
-      <span>{{ this.error }}</span>
-    </div>
+    <template v-if="error">
+      <h3>You were not able to login</h3>
+      <div class="alert alert-danger">
+        <span>{{ this.error }}</span>
+      </div>
+      <a @click.prevent="tryAgain" class="btn btn-lg btn-primary">Try again</a>
+    </template>
     <div v-else class="text-center">
       <p>You will be logged in with your Personify account, please wait...</p>
       <Spinner></Spinner>
@@ -23,16 +27,25 @@ export default {
       error: '',
     };
   },
-  async mounted() {
-    await this.$store
-      .dispatch('personifyAuthorize')
-      .then(() => {
-        this.$router.push({ name: 'Home' }).catch(() => {});
-      })
-      .catch((error) => {
-        this.error = error.response ? error.response.data.message : 'Something went wrong!';
-        throw error;
-      });
+  mounted() {
+    this.runLoginProcess();
+  },
+  methods: {
+    async runLoginProcess() {
+      await this.$store
+        .dispatch('personifyAuthorize')
+        .then(() => {
+          this.$router.push({ name: 'Home' }).catch(() => {});
+        })
+        .catch((error) => {
+          this.error = error.response ? error.response.data.message : 'Something went wrong!';
+          throw error;
+        });
+    },
+    tryAgain() {
+      this.error = '';
+      this.runLoginProcess();
+    },
   },
 };
 </script>
