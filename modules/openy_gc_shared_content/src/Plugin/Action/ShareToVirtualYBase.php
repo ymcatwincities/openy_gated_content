a62b819cf888b7bd6fd2f234d98584cd9d474793
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2020-08-07 12:26:28 +0300

    phpcs comments

--- modules/openy_gc_shared_content/src/Plugin/Action/ShareToVirtualYBase.php
+++ modules/openy_gc_shared_content/src/Plugin/Action/ShareToVirtualYBase.php
@@ -5,7 +5,7 @@ namespace Drupal\openy_gc_shared_content\Plugin\Action;
 use Drupal\Core\Field\FieldUpdateActionBase;
 
 /**
- *
+ * Base class for Share to Virtual Y mass actions
  */
 class ShareToVirtualYBase extends FieldUpdateActionBase {
 
