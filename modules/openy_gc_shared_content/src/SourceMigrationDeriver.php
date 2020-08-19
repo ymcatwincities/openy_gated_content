c1be686b6e2ebf2e0c5ad5df80bddd8cda3fedf0
Anatoliy Polyakov <anpolimus@gmail.com>
2020-08-20 01:05:39 +0300

    Updated deriver for all shared migrations

--- modules/openy_gc_shared_content/src/SourceMigrationDeriver.php
+++ modules/openy_gc_shared_content/src/SourceMigrationDeriver.php
@@ -30,15 +30,14 @@ class SourceMigrationDeriver extends DeriverBase implements DeriverInterface {
       'sort[sortByDate][direction]' => 'DESC',
       'filter[status]' => 1,
       //@TODO add shared_content_filter once it will be at test servers.
-      //'limit' => 1000
+      //'limit' => 100
     ];
 
-    $jsonapi_uri = '/jsonapi/node/gc_video?' . http_build_query($params);
+    $jsonapi_uri = '/jsonapi/node/' . $base_plugin_definition['source']['entity_type'] . '?' . http_build_query($params);
 
     foreach ($urls as $url) {
       $url_long = $url . $jsonapi_uri;
       $derivative = $this->getDerivativeValues($base_plugin_definition, $url_long, $url);
-      //dump($url_long);
       $this->derivatives[$this->getKey($url)] = $derivative;
     }
 
@@ -50,14 +49,17 @@ class SourceMigrationDeriver extends DeriverBase implements DeriverInterface {
    *
    * @param array $base_plugin_definition
    *   Plugin settings.
+   * @param $url_long
+   *   Url with request part
    * @param $url
    *   Dynamic url for every Virtual Y content source.
    *
-   * @return arrayx
+   * @return array
    */
   private function getDerivativeValues(array $base_plugin_definition, $url_long, $url) {
 
     $base_plugin_definition['source']['urls'] = $url_long;
+
     if (!empty($base_plugin_definition['process']['field_gc_video_media'])) {
       $migration = str_replace(
         'REPLACE_ME',
@@ -68,13 +70,65 @@ class SourceMigrationDeriver extends DeriverBase implements DeriverInterface {
       $base_plugin_definition['migration_dependencies']['required'] = [$migration];
     }
 
+    if (!empty($base_plugin_definition['process']['field_vy_blog_image'])) {
+      $migration = str_replace(
+        'REPLACE_ME',
+        $this->getKey($url),
+        $base_plugin_definition['process']['field_vy_blog_image']['migration']
+      );
+      $base_plugin_definition['process']['field_vy_blog_image']['migration'] = $migration;
+      $base_plugin_definition['migration_dependencies']['required'] = [$migration];
+    }
+
+    if (!empty($base_plugin_definition['process']['field_media_image'])) {
+      $migration = str_replace(
+        'REPLACE_ME',
+        $this->getKey($url),
+        $base_plugin_definition['process']['field_media_image']['migration']
+      );
+      $base_plugin_definition['process']['field_media_image']['migration'] = $migration;
+      $base_plugin_definition['migration_dependencies']['required'] = [$migration];
+    }
+
+    if (!empty($base_plugin_definition['source']['constants']['DOMAIN'])) {
+      $base_plugin_definition['source']['constants']['DOMAIN'] = $url;
+    }
+
+    if (!empty($base_plugin_definition['process']['field_gc_video_image'])) {
+      $migration = str_replace(
+        'REPLACE_ME',
+        $this->getKey($url),
+        $base_plugin_definition['process']['field_gc_video_image']['migration']
+      );
+      $base_plugin_definition['process']['field_gc_video_image']['migration'] = $migration;
+      $base_plugin_definition['migration_dependencies']['required'][] = $migration;
+    }
+
     return $base_plugin_definition;
   }
 
+  /**
+   * Helper function that checks if we need includes to jsonapi request.
+   *
+   * @param $base_plugin_definition
+   *   Migration array.
+   *
+   * @return array
+   *   Includes array for JSON:API
+   */
   private function getRemoteRelationshipsList($base_plugin_definition) {
     return !empty($base_plugin_definition['source']['json_includes']) ? $base_plugin_definition['source']['json_includes'] : [];
   }
 
+  /**
+   * Helper function that prepare entity key from url.
+   *
+   * @param $url
+   *   Url to source.
+   *
+   * @return mixed
+   *   Machine name url for key.
+   */
   private function getKey($url) {
     $url_key = str_replace('http://', '', $url);
     $url_key = str_replace('.', '_', $url_key);
