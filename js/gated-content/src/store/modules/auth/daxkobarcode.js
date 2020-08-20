53b13e671aed8eca78ed7ea340e98a171e1e1378
Avi Schwab <avi.schwab@ymca.net>
2020-08-19 22:59:07 -0500

    Initial pass at connecting Vue to Drupal

--- js/gated-content/src/store/modules/auth/daxkobarcode.js
+++ js/gated-content/src/store/modules/auth/daxkobarcode.js
@@ -5,10 +5,40 @@ export default {
     config: null,
   },
   actions: {
-    daxkobarcodeAuthorize(context) {
-      console.log('You are authorized now!');
-      // Call the base auth authorize action.
-      context.dispatch('authorize', {});
+    async daxkobarcodeAuthorize(context, data) {
+      console.log("Post to: ", context.getters.getDaxkoBarcodeConfig.barcodeValidate);
+
+      return client
+        .get('session/token')
+        .then((response) => client({
+          url: context.getters.getDaxkoBarcodeConfig.barcodeValidate,
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': response.data,
+          },
+          params: {
+            _format: 'json',
+          },
+          data: {
+            barcode: data.barcode
+          }
+        })
+          .then((response2) => {
+            console.log(response2.data);
+            if (response2.status === 200) {
+              // Call the base auth authorize action.
+              context.dispatch('authorize', response2.data.user);
+            }
+            return response2;
+          })
+          .catch((error) => {
+            console.log(error.response.data.message);
+            throw error;
+          }))
+        .catch(e => {
+          this.errors.push(e);
+        });
     },
     daxkobarcodeLogout(context) {
       console.log('Logging you out');
