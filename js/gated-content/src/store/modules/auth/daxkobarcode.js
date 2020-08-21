d73ee39a4fa864fe0c7f7dec0bb9af0eded80779
Avi Schwab <avi.schwab@ymca.net>
2020-08-20 20:46:22 -0500

    Clean up logging

--- js/gated-content/src/store/modules/auth/daxkobarcode.js
+++ js/gated-content/src/store/modules/auth/daxkobarcode.js
@@ -6,8 +6,6 @@ export default {
   },
   actions: {
     async daxkobarcodeAuthorize(context, data) {
-      console.log("Post to: ", context.getters.getDaxkoBarcodeConfig.barcodeValidate);
-
       return client
         .get('session/token')
         .then((response) => client({
@@ -26,7 +24,6 @@ export default {
           }
         })
           .then((response2) => {
-            console.log(response2.data);
             if (response2.status === 200) {
               // Call the base auth authorize action.
               context.dispatch('authorize', response2.data.user);
@@ -34,7 +31,6 @@ export default {
             return response2;
           })
           .catch((error) => {
-            console.log(error.response.data.message);
             throw error;
           }))
         .catch(error => {
@@ -42,12 +38,10 @@ export default {
         });
     },
     daxkobarcodeLogout(context) {
-      console.log('Logging you out');
       // Call the base auth logout action.
       context.dispatch('logout');
     },
     daxkobarcodeConfigure(context, config) {
-      console.log('Setting configuration', config);
       context.commit('setDaxkoBarcodeConfig', config);
     },
   },
