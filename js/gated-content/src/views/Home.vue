551cfa24ed1f64c78c8fcac2086b25baccfe06e0
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-09-08 18:18:24 +0300

    Wait until settings will be available globally in app

--- js/gated-content/src/views/Home.vue
+++ js/gated-content/src/views/Home.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="home">
-    <div v-if="!config" class="text-center">
-      <Spinner></Spinner>
-    </div>
-    <div v-else>
-      <VideoListing
-        :featured="true"
-        :viewAll="true"
-        :limit="6"
-        :title="config.components.gc_video.title"
-        v-if="isActive('gc_video')"
-      />
-      <EventListing
-        :featured="true"
-        :viewAll="true"
-        :limit="6"
-        :msg="'Live streams not found.'"
-        :title="config.components.live_stream.title"
-        v-if="isActive('live_stream')"
-      />
-      <EventListing
-        :title="config.components.virtual_meeting.title"
-        :featured="true"
-        :viewAll="true"
-        :limit="6"
-        :eventType="'virtual_meeting'"
-        :msg="'Virtual Meetings not found.'"
-        v-if="isActive('virtual_meeting')"
-      />
-      <BlogListing
-        :featured="false"
-        :viewAll="true"
-        :limit="6"
-        :title="config.components.vy_blog_post.title"
-        v-if="isActive('vy_blog_post')"
-      />
-    </div>
+    <VideoListing
+      :featured="true"
+      :viewAll="true"
+      :limit="6"
+      :title="config.components.gc_video.title"
+      v-if="isActive('gc_video')"
+    />
+    <EventListing
+      :featured="true"
+      :viewAll="true"
+      :limit="6"
+      :msg="'Live streams not found.'"
+      :title="config.components.live_stream.title"
+      v-if="isActive('live_stream')"
+    />
+    <EventListing
+      :title="config.components.virtual_meeting.title"
+      :featured="true"
+      :viewAll="true"
+      :limit="6"
+      :eventType="'virtual_meeting'"
+      :msg="'Virtual Meetings not found.'"
+      v-if="isActive('virtual_meeting')"
+    />
+    <BlogListing
+      :featured="false"
+      :viewAll="true"
+      :limit="6"
+      :title="config.components.vy_blog_post.title"
+      v-if="isActive('vy_blog_post')"
+    />
   </div>
 </template>
 
