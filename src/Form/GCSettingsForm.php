a2a11f0fb2e8caf8f632b5722681249bd4204564
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-09-22 20:04:46 +0300

    [VYMCA-79] Add urls to app settings

--- src/Form/GCSettingsForm.php
+++ src/Form/GCSettingsForm.php
@@ -76,6 +76,7 @@ class GCSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Items limit for blocks with pager.'),
       '#type' => 'number',
       '#default_value' => $config->get('pager_limit') ?? self::PAGER_LIMIT_DEFAULT,
+      '#required' => TRUE,
     ];
 
     $form['app_settings']['components'] = [
@@ -125,12 +126,14 @@ class GCSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#title' => 'Virtual Y Landing Page url',
       '#default_value' => $config->get('virtual_y_url'),
+      '#required' => TRUE,
     ];
 
     $form['app_settings']['virtual_y_login_url'] = [
       '#type' => 'textfield',
       '#title' => 'Virtual Y Login Landing Page url',
       '#default_value' => $config->get('virtual_y_login_url'),
+      '#required' => TRUE,
     ];
 
     $form['actions']['#type'] = 'actions';
