2222914aef3e313b381576c2b6bca08e3f979b27
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-09-23 17:53:24 +0300

    [VYMCA-79] Updates fixes

--- modules/openy_gc_auth/modules/openy_gc_auth_custom/openy_gc_auth_custom.post_update.php
+++ modules/openy_gc_auth/modules/openy_gc_auth_custom/openy_gc_auth_custom.post_update.php
@@ -7,7 +7,17 @@
 
 /**
  * Delete deprecated gc_auth_custom_user instances.
+ *
+ * Run "drush mim gc_auth_custom_users" after this update to restore customers
+ * in user entity.
  */
 function openy_gc_auth_custom_post_update_delete_custom_users(&$sandbox) {
   \Drupal::database()->truncate('gc_auth_custom_user')->execute();
+  $schema = \Drupal::database()->schema();
+  if ($schema->tableExists('migrate_map_gc_auth_custom_users')) {
+    \Drupal::database()->truncate('migrate_map_gc_auth_custom_users')->execute();
+  }
+  if ($schema->tableExists('migrate_message_gc_auth_custom_users')) {
+    \Drupal::database()->truncate('migrate_message_gc_auth_custom_users')->execute();
+  }
 }
