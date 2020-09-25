f12255fcabf643343a5004f5dfa0cdf6457f7fdb
Andrey Tiupa <andrey@imagexmedia.com>
2020-09-25 14:41:35 +0300

    Fix cs

--- modules/openy_gc_log/src/Logger.php
+++ modules/openy_gc_log/src/Logger.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\openy_gc_log;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\openy_gc_log\Entity\LogEntity;
 use Drupal\user\Entity\User;
@@ -19,13 +20,23 @@ class Logger {
   private $logger;
 
   /**
+   * The entity type targeted by this resource.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * LogArchiver constructor.
    *
    * @param \Drupal\Core\Logger\LoggerChannel $logger
    *   LoggerChannel.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
-  public function __construct(LoggerChannel $logger) {
+  public function __construct(LoggerChannel $logger, EntityTypeManagerInterface $entity_type_manager) {
     $this->logger = $logger;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -34,15 +45,15 @@ class Logger {
    * @param array $params
    *   Array with parameters.
    *
-   * @return boolean|Entity\LogEntity
+   * @return bool|Entity\LogEntity
    *   Return false or log_entity.
    */
-  public function addLog($params) {
+  public function addLog(array $params) {
     try {
       $log = new LogEntity([], 'log_entity');
       // Ensure we have email in $params.
       if (empty($params['email']) && !empty($params['uid'])) {
-        $user = User::load($params['uid']);
+        $user = $this->entityTypeManager->getStorage('user')->load($params['uid']);
         $params['email'] = $user->getEmail();
         unset($params['uid']);
       }
