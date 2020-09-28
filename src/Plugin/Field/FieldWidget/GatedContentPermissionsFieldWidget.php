6eeb82c46affabb5b45268581ed44ea0a45ec51c
Anatoliy Polyakov <anpolimus@gmail.com>
2020-09-28 10:08:37 +0300

    Added DI

--- src/Plugin/Field/FieldWidget/GatedContentPermissionsFieldWidget.php
+++ src/Plugin/Field/FieldWidget/GatedContentPermissionsFieldWidget.php
@@ -3,10 +3,13 @@
 namespace Drupal\openy_gated_content\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\user\Entity\Role;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Plugin implementation of the Gated Content permissions widget.
@@ -19,9 +22,58 @@ use Drupal\user\Entity\Role;
  *   }
  * )
  */
-class GatedContentPermissionsFieldWidget extends WidgetBase {
+class GatedContentPermissionsFieldWidget extends WidgetBase implements ContainerFactoryPluginInterface {
 
   use StringTranslationTrait;
+
+  /**
+   * Entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs widget plugin.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the widget is associated.
+   * @param array $settings
+   *   The widget settings.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager service.
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    array $third_party_settings,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -36,7 +88,7 @@ class GatedContentPermissionsFieldWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
 
-    $roles = Role::loadMultiple();
+    $roles = $this->entityTypeManager->getStorage('user_role')->loadMultiple();
     $select_options = [];
     foreach ($roles as $role_key => $role) {
       if ((strpos($role_key, 'virtual_y') !== FALSE)
@@ -65,6 +117,7 @@ class GatedContentPermissionsFieldWidget extends WidgetBase {
 
     $new_value = '';
     foreach ($values as $value) {
+      unset($value['_original_delta']);
       $new_value .= implode(',', $value);
     }
     return $new_value;
