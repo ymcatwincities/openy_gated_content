f52e9bc199118251e3187834ecb3321cab3ad339
Anatoliy Polyakov <anpolimus@gmail.com>
2020-09-29 14:07:38 +0300

    Added post update for automatic update of all virtual y content items with default permissions field data

--- openy_gated_content.post_update.php
+++ openy_gated_content.post_update.php
@@ -53,3 +53,62 @@ function openy_gated_content_post_update_create_login_page(&$sandbox) {
   $gated_content_settings->set('virtual_y_login_url', $gated_content_login_alias);
   $gated_content_settings->save();
 }
+
+/**
+ * Helper function for post updates of permissions field.
+ */
+function _openy_gated_content_permissions(&$sandbox, string $entity_type, string $bundle) {
+  if (!isset($sandbox['progress'])) {
+    $sandbox['progress'] = 0;
+    $sandbox['current'] = 0;
+    $sandbox['max'] = \Drupal::entityQuery($entity_type)
+      ->condition('type', $bundle)
+      ->count()
+      ->execute();
+  }
+  $ids = \Drupal::entityQuery($entity_type)
+    ->condition('type', $bundle)
+    ->condition('nid', $sandbox['current'], '>')
+    ->range(0, 5)
+    ->sort('nid')
+    ->execute();
+  $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($ids);
+  foreach ($nodes as $node) {
+    $node->field_vy_permission->value = 'virtual_y';
+    $node->save();
+    $sandbox['progress']++;
+    $sandbox['current'] = $node->id();
+  }
+  $sandbox['#finished'] = $sandbox['progress'] >= $sandbox['max'] ? TRUE : $sandbox['progress'] / $sandbox['max'];
+  if ($sandbox['#finished']) {
+    return t('Fields data were migrated for @count entities', ['@count' => $sandbox['max']]);
+  }
+}
+
+/**
+ * Update all existed Virtual Y Videos with default permissions field.
+ */
+function openy_gated_content_post_update_permissions_video(&$sandbox) {
+  _openy_gated_content_permissions($sandbox, 'node', 'gc_video');
+}
+
+/**
+ * Update all existed Virtual Y Blog Posts with default permissions field.
+ */
+function openy_gated_content_post_update_permissions_blog_posts(&$sandbox) {
+  _openy_gated_content_permissions($sandbox, 'node', 'vy_blog_post');
+}
+
+/**
+ * Update all existed Virtual Y Online streams with default permissions field.
+ */
+function openy_gated_content_post_update_eventseries_livestream(&$sandbox) {
+  _openy_gated_content_permissions($sandbox, 'eventseries', 'live_stream');
+}
+
+/**
+ * Update all existed Virtual Y Virtual meetings with default permissions field.
+ */
+function openy_gated_content_post_update_eventseries_meeting(&$sandbox) {
+  _openy_gated_content_permissions($sandbox, 'eventseries', 'virtual_meeting');
+}
