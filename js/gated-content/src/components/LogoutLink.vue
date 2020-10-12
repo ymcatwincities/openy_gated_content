d6b5acd4bdaf48d6ac96579401b7f4da056b3aa4
Andrey Tiupa <andrey@imagexmedia.com>
2020-10-12 16:51:58 +0300

    VY: Make logout link configurable

--- js/gated-content/src/components/LogoutLink.vue
+++ js/gated-content/src/components/LogoutLink.vue
@@ -1,5 +1,16 @@
 <template>
   <span class="menu">
-    <a href="/user/logout">Sign out</a>
+    <a :href="logoutUrl">Sign out</a>
   </span>
 </template>
+
+<script>
+  export default {
+    name: 'LogoutLink',
+    data() {
+      return {
+        logoutUrl: window.drupalSettings.openy_gated_content.logoutUrl,
+      };
+    }
+  };
+</script>
