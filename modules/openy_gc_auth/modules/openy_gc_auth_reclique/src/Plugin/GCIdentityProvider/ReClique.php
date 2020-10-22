9fc93e6077723841a4a28959aff254e05fb95d10
Andrey Tiupa <andrey@imagexmedia.com>
2020-10-22 14:42:47 +0300

    Add persmissions mapping to reclique provider

--- modules/openy_gc_auth/modules/openy_gc_auth_reclique/src/Plugin/GCIdentityProvider/ReClique.php
+++ modules/openy_gc_auth/modules/openy_gc_auth_reclique/src/Plugin/GCIdentityProvider/ReClique.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\openy_gc_auth_reclique\Plugin\GCIdentityProvider;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\openy_gc_auth\GCIdentityProviderPluginBase;
 
@@ -15,6 +16,8 @@ use Drupal\openy_gc_auth\GCIdentityProviderPluginBase;
  * )
  */
 class ReClique extends GCIdentityProviderPluginBase {
+
+  use DependencySerializationTrait;
 
   const DEFAULT_LINK_LIFE_TIME = 14400;
 
@@ -38,6 +41,52 @@ class ReClique extends GCIdentityProviderPluginBase {
     $config = $this->getConfiguration();
     $form = parent::buildConfigurationForm($form, $form_state);
     $form['#tree'] = TRUE;
+    $form['permissions_mapping'] = [
+      '#title' => $this->t('Permissions mapping'),
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#prefix' => '<div id="permissions-mapping-fieldset-wrapper">',
+      '#suffix' => '</div>',
+    ];
+    $permissions_mapping = explode(';', $config['permissions_mapping']);
+    if (!$form_state->has('permissions_mapping_items_count')) {
+      $form_state->set('permissions_mapping_items_count', count($permissions_mapping));
+    }
+    $permissions_mapping_items = $form_state->get('permissions_mapping_items_count');
+    $roles = $this->gcUserService->getRoles();
+    for ($i = 0; $i < $permissions_mapping_items; $i++) {
+      $role = isset($permissions_mapping[$i]) ? explode(':', $permissions_mapping[$i]) : '';
+      $form['permissions_mapping'][$i]['permissions_mapping_reclique_role'] = [
+        '#title' => $this->t('ReClique membership'),
+        '#type' => 'textfield',
+        '#default_value' => isset($role[0]) ? $role[0] : '',
+        '#size' => 30,
+        '#prefix' => '<div class="container-inline">',
+      ];
+
+      $form['permissions_mapping'][$i]['permissions_mapping_role'] = [
+        '#title' => $this->t('Virtual Y role'),
+        '#type' => 'select',
+        '#options' => ['' => $this->t('None')] + $roles,
+        '#default_value' => isset($role[1]) ? $role[1] : '',
+        '#suffix' => '</div>',
+      ];
+    }
+
+    $form['permissions_mapping']['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['permissions_mapping']['actions']['add'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add one more'),
+      '#submit' => [[get_class($this), 'addOne']],
+      '#ajax' => [
+        'callback' => [get_class($this), 'addmoreCallback'],
+        'wrapper' => 'permissions-mapping-fieldset-wrapper',
+      ],
+    ];
+
 
     $form['verification_url'] = [
       '#title' => $this->t('Verification URL'),
@@ -126,6 +175,36 @@ class ReClique extends GCIdentityProviderPluginBase {
   }
 
   /**
+   * Add more item.
+   *
+   * @param array $form
+   *   Form data.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form state.
+   */
+  public static function addOne(array &$form, FormStateInterface $form_state) {
+    $permissions_mapping_items = $form_state->get('permissions_mapping_items_count');
+    $add_button = $permissions_mapping_items + 1;
+    $form_state->set('permissions_mapping_items_count', $add_button);
+    $form_state->setRebuild();
+  }
+
+  /**
+   * Add more callback.
+   *
+   * @param array $form
+   *   Form data.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form state.
+   *
+   * @return mixed
+   *   Return array with data.
+   */
+  public static function addmoreCallback(array &$form, FormStateInterface $form_state) {
+    return $form['settings']['permissions_mapping'];
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
@@ -139,6 +218,12 @@ class ReClique extends GCIdentityProviderPluginBase {
       $this->configuration['email_verification_link_life_time'] = $form_state->getValue('settings')['verification']['email_verification_link_life_time'];
       $this->configuration['email_verification_text'] = !empty($form_state->getValue('settings')['verification']['email_verification_text']) ? $form_state->getValue('settings')['verification']['email_verification_text']['value'] : '';
       $this->configuration['verification_message'] = !empty($form_state->getValue('settings')['verification']['verification_message']) ? $form_state->getValue('settings')['verification']['verification_message']['value'] : '';
+      foreach ($form_state->getValue('settings')['permissions_mapping'] as $mapping) {
+        if (!empty($mapping['permissions_mapping_reclique_role'])) {
+          $permissions_mapping[] = $mapping['permissions_mapping_reclique_role'] . ':' . $mapping['permissions_mapping_role'];
+        }
+      }
+      $this->configuration['permissions_mapping'] = !empty($permissions_mapping) ? implode(';', $permissions_mapping) : '';
       parent::submitConfigurationForm($form, $form_state);
     }
   }
