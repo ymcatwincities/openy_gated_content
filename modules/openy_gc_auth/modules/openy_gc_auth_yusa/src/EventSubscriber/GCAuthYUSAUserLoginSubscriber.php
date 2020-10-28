a47d27227f9e03513626a0b775844c8d2795e231
Andrey Tiupa <andrey@imagexmedia.com>
2020-10-28 17:02:04 +0200

    VY: Make subscribers specific for enabled auth provider

--- modules/openy_gc_auth/modules/openy_gc_auth_yusa/src/EventSubscriber/GCAuthYUSAUserLoginSubscriber.php
+++ modules/openy_gc_auth/modules/openy_gc_auth_yusa/src/EventSubscriber/GCAuthYUSAUserLoginSubscriber.php
@@ -48,29 +48,31 @@ class GCAuthYUSAUserLoginSubscriber implements EventSubscriberInterface {
    *   Event object.
    */
   public function onUserLogin(GCUserLoginEvent $event) {
-    $permissions_mapping = $this->configFactory->get('openy_gc_auth.provider.yusa')->get('permissions_mapping');
-    if ($event->account instanceof User && !empty($event->extraData)) {
-      $account = $event->account;
-      if (isset($event->extraData['Memberships'])) {
-        $account_roles = $account->getRoles();
-        // Remove all virtual_y roles (in case if any changes in membership).
-        foreach ($account_roles as $account_role) {
-          if (strstr($account_role, 'virtual_y')) {
-            $account->removeRole($account_role);
-          }
-        }
-        $user_memberships = $event->extraData['Memberships'];
-        $permissions_mapping = explode(';', $permissions_mapping);
-        foreach ($permissions_mapping as $mapping) {
-          $role = explode(':', $mapping);
-          // Compare mapping roles with user membership.
-          foreach ($user_memberships as $user_membership) {
-            if (isset($role[0]) && $role[0] == $user_membership && isset($role[1])) {
-              $account->addRole($role[1]);
+    if ($this->configFactory->get('openy_gc_auth.settings')->get('active_provider') == 'yusa') {
+      $permissions_mapping = $this->configFactory->get('openy_gc_auth.provider.yusa')->get('permissions_mapping');
+      if ($event->account instanceof User && !empty($event->extraData)) {
+        $account = $event->account;
+        if (isset($event->extraData['Memberships'])) {
+          $account_roles = $account->getRoles();
+          // Remove all virtual_y roles (in case if any changes in membership).
+          foreach ($account_roles as $account_role) {
+            if (strstr($account_role, 'virtual_y')) {
+              $account->removeRole($account_role);
             }
           }
+          $user_memberships = $event->extraData['Memberships'];
+          $permissions_mapping = explode(';', $permissions_mapping);
+          foreach ($permissions_mapping as $mapping) {
+            $role = explode(':', $mapping);
+            // Compare mapping roles with user membership.
+            foreach ($user_memberships as $user_membership) {
+              if (isset($role[0]) && $role[0] == $user_membership && isset($role[1])) {
+                $account->addRole($role[1]);
+              }
+            }
+          }
+          $account->save();
         }
-        $account->save();
       }
     }
   }
