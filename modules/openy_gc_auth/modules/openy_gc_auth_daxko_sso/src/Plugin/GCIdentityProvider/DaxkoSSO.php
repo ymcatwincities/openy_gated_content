61f24218cee532ad4fa58dd1738596de3f3c0a75
Anatoliy Polyakov <anpolimus@gmail.com>
2020-11-04 12:19:25 +0200

    Forced use of no-cache at the Daxko redirect response.

--- modules/openy_gc_auth/modules/openy_gc_auth_daxko_sso/src/Plugin/GCIdentityProvider/DaxkoSSO.php
+++ modules/openy_gc_auth/modules/openy_gc_auth_daxko_sso/src/Plugin/GCIdentityProvider/DaxkoSSO.php
@@ -150,7 +150,18 @@ class DaxkoSSO extends GCIdentityProviderPluginBase {
    * {@inheritdoc}
    */
   public function getLoginForm() {
-    return new RedirectResponse(Url::fromRoute('openy_gc_auth_daxko_sso.daxko_link_controller_hello')->toString());
+
+    // Forcing no-cache at redirect headers.
+    $headers = [
+      'Cache-Control' => 'no-cache',
+    ];
+    
+    $response = new RedirectResponse(
+      Url::fromRoute('openy_gc_auth_daxko_sso.daxko_link_controller_hello')->toString(),
+      302,
+      $headers
+    );
+    $response->send();
   }
 
 }
