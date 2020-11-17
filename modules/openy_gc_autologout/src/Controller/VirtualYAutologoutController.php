67e3e44f771d1eb30284dcca0dd1319c5df9aef9
Andrey Tiupa <andrey@imagexmedia.com>
2020-11-17 14:13:56 +0200

    VY: Add Autologout functionality

---
+++ modules/openy_gc_autologout/src/Controller/VirtualYAutologoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\openy_gc_autologout\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Returns responses for openy_gc_autologout module routes.
+ */
+class VirtualYAutologoutController extends ControllerBase {
+
+  public function logOut() {
+    user_logout();
+    return new JsonResponse([
+      'logout' => 'true',
+    ]);
+  }
+
+}
