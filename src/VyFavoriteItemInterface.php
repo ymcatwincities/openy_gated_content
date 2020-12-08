cf952f7a8bf77e0d3d35243ab99fc4e6d81cd21c
Igor Karpilenko <karpylenko.ihor@gmail.com>
2020-12-08 13:50:34 +0200

    [VYMCA-104] Add methods to vy_favorite_item entity interface

--- src/VyFavoriteItemInterface.php
+++ src/VyFavoriteItemInterface.php
@@ -6,8 +6,32 @@ use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\user\EntityOwnerInterface;
 
 /**
- * Provides an interface defining a path_alias entity.
+ * Provides an interface defining a vy_favorite_item entity.
  */
 interface VyFavoriteItemInterface extends ContentEntityInterface, EntityOwnerInterface {
+
+  /**
+   * Returns the referenced entity bundle.
+   *
+   * @return string
+   *   The referenced entity bundle.
+   */
+  public function getRefEntityBundle();
+
+  /**
+   * Returns the referenced entity type.
+   *
+   * @return string
+   *   The referenced entity type.
+   */
+  public function getRefEntityType();
+
+  /**
+   * Returns the referenced entity ID.
+   *
+   * @return string
+   *   The referenced entity ID.
+   */
+  public function getRefEntityId();
 
 }
