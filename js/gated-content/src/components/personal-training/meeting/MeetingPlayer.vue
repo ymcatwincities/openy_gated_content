b8a31bfcf4d000f631ac8b72189aac9f6b06eacb
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2021-04-18 20:33:57 +0300

    accordion for description, stream background

--- js/gated-content/src/components/personal-training/meeting/MeetingPlayer.vue
+++ js/gated-content/src/components/personal-training/meeting/MeetingPlayer.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="meeting-player" :class="view">
     <video
-      v-if="partnerMediaStream"
       class="partner"
-      :srcObject.prop="partnerMediaStream"
+      :srcObject.prop="partnerMediaStream ? partnerMediaStream : undefined"
       autoplay="autoplay"
+      :style="partnerVideoStyle"
     ></video>
     <video
-      v-if="localMediaStream"
       class="local"
-      :srcObject.prop="localMediaStream"
+      :srcObject.prop="localMediaStream ? localMediaStream : undefined"
       autoplay="autoplay"
       muted="muted"
       :volume.prop="0"
@@ -21,6 +20,16 @@
 import { mapGetters } from 'vuex';
 
 export default {
+  data() {
+    return {
+      partnerVideoStyle: {
+        width: undefined,
+      },
+    };
+  },
+  updated() {
+    this.partnerVideoStyle.width = this.$el.querySelector('.partner').clientHeight * 1.777777778;
+  },
   computed: {
     ...mapGetters([
       'view',
