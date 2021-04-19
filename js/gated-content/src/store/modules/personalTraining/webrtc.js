161abae5f47c262e46911ea806f7f4fb51092008
Anatoliy Polyakov <anpolimus@gmail.com>
2021-04-19 23:54:03 +0300

    Test turn server

--- js/gated-content/src/store/modules/personalTraining/webrtc.js
+++ js/gated-content/src/store/modules/personalTraining/webrtc.js
@@ -43,9 +43,9 @@ export default {
           iceServers: [
             { url: 'stun:stun.l.google.com:19302' },
             {
-              url: 'turn:192.158.29.39:3478?transport=udp',
-              credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-              username: '28224511:1379330808',
+              url: 'turn:numb.viagenie.ca',
+              credential: 'muazkh',
+              username: 'webrtc@live.com',
             },
           ],
         },
