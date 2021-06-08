e8259477e45f210b0ab5aeed3a30aa1f54fd7da6
Vladyslav Solodovnyk <vladyslav.solodovnyk@fivejars.com>
2021-06-08 14:44:58 +0300

    [PRODDEV-189] Add carnation_banner image style and use it for Blog and Virtual Meeting views' covers, refactor existing code

--- js/gated-content/src/components/blog/BlogTeaser.vue
+++ js/gated-content/src/components/blog/BlogTeaser.vue
@@ -4,7 +4,7 @@
     :routeName="'BlogPage'"
     :id="blog.id"
     :component="'vy_blog_post'"
-    :image="image"
+    :image="blog.attributes['field_vy_blog_image.field_media_image']"
   >
     <template>
       <div class="title">{{ blog.attributes.title }}</div>
@@ -33,16 +33,6 @@ export default {
     blog: {
       type: Object,
       required: true,
-    },
-  },
-  computed: {
-    image() {
-      if (!this.blog.attributes['field_vy_blog_image.field_media_image']) {
-        return null;
-      }
-
-      return this.blog.attributes['field_vy_blog_image.field_media_image']
-        .image_style_uri[0].gated_content_teaser;
     },
   },
 };
