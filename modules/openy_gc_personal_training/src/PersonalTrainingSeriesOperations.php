15793423c072d2ef819ca4ca63eec6c90e484b2b
Vladyslav Solodovnyk <vladyslav.solodovnyk@fivejars.com>
2021-07-02 17:13:40 +0300

    [PRODDEV-338] Remove VyFavoriteItem on referenced entities delete.

--- modules/openy_gc_personal_training/src/PersonalTrainingSeriesOperations.php
+++ modules/openy_gc_personal_training/src/PersonalTrainingSeriesOperations.php
@@ -3,24 +3,16 @@
 namespace Drupal\openy_gc_personal_training;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\openy_gc_personal_training\Entity\PersonalTrainingInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Defines a class for reacting to Personal Trainings events.
+ * Defines a class for reacting to Personal Trainings Series events.
  */
 class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
 
   use StringTranslationTrait;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
 
   /**
    * Personal Trainings Series Manager service.
@@ -32,16 +24,12 @@ class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
   /**
    * PersonalTrainingSeriesOperations constructor.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   Entity type manager.
    * @param \Drupal\openy_gc_personal_training\PersonalTrainingSeriesManagerInterface $personal_training_series_manager
-   *   Livestream Manager service.
+   *   Personal Trainings Series manager service.
    */
   public function __construct(
-    EntityTypeManagerInterface $entity_type_manager,
     PersonalTrainingSeriesManagerInterface $personal_training_series_manager
   ) {
-    $this->entityTypeManager = $entity_type_manager;
     $this->personalTrainingSeriesManager = $personal_training_series_manager;
   }
 
@@ -50,7 +38,6 @@ class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity_type.manager'),
       $container->get('personal_training_series.manager')
     );
   }
@@ -66,7 +53,7 @@ class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
    * @throws \Drupal\Core\Entity\EntityMalformedException
    * @throws \Drupal\Core\Entity\EntityStorageException
    *
-   * @see hook_node_insert()
+   * @see hook_ENTITY_TYPE_insert()
    */
   public function entityInsert(PersonalTrainingInterface $training_series): void {
     // Generate personal_training instances for a new training_series.
@@ -81,7 +68,7 @@ class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
    *
    * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    *
-   * @see hook_node_update()
+   * @see hook_ENTITY_TYPE_update()
    */
   public function entityUpdate(PersonalTrainingInterface $training_series) {
     if ($training_series->original->field_schedule->equals($training_series->field_schedule) &&
@@ -102,11 +89,11 @@ class PersonalTrainingSeriesOperations implements ContainerInjectionInterface {
    * Implements logic for the ENTITY_TYPE_delete hook.
    *
    * @param \Drupal\openy_gc_personal_training\Entity\PersonalTrainingInterface $training_series
-   *   Node object that was just deleted.
+   *   Entity that was just deleted.
    *
    * @throws \Drupal\Core\Entity\EntityStorageException
    *
-   * @see hook_node_delete()
+   * @see hook_ENTITY_TYPE_delete()
    */
   public function entityDelete(PersonalTrainingInterface $training_series) {
     // Delete personal_training instances related to training_series.
