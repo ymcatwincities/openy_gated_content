810f06e51e617e0e87a0e29144293a1acd5b2a9e
Yaroslav Levchenko <yaroslavl@imagexmedia.com>
2021-08-10 21:00:04 +0300

    PRODDEV-272 Adjust error message construction in order to avoid having 1 instead of the actual message

--- modules/openy_gc_auth/modules/openy_gc_auth_avocado_sso/src/SSOClient.php
+++ modules/openy_gc_auth/modules/openy_gc_auth_avocado_sso/src/SSOClient.php
@@ -312,8 +312,13 @@ class SSOClient {
         !isset($body->data)
         || $body->data->eventLogMessage !== "Success"
       ) {
+        $this->logger->debug(print_r($body, TRUE));
         $error_message = isset($body->data->eventLogMessage) ?? $body->message;
-        $this->logger->error("Error happened during create user log process: $error_message");
+        $this->logger->error(
+          "Error happened during create user log process: %error_message", [
+            '%error_message' => print_r($error_message, TRUE),
+          ]
+        );
 
         return FALSE;
       }
