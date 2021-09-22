0cf08bb157ba30d288100335c106edd647f8b447
Yaroslav Levchenko <yaroslavl@imagexmedia.com>
2021-09-22 19:17:01 +0300

    PRODDEV-439 Add hook_update to update existed data and add duration reference

--- modules/openy_gc_storage/openy_gc_storage.post_update.php
+++ modules/openy_gc_storage/openy_gc_storage.post_update.php
@@ -90,3 +90,165 @@ function openy_gc_storage_post_update_migrate_eventseries_instructors(&$sandbox)
 function openy_gc_storage_post_update_migrate_eventinstance_instructors(&$sandbox) {
   _openy_gc_storage_migrate_instructors($sandbox, 'eventinstance', 'field_ls_host_name');
 }
+
+/**
+ * Create terms for Virtual Y Duration.
+ */
+function openy_gc_storage_post_update_create_durations(&$sandbox) {
+  if (!isset($sandbox['durations'])) {
+    $sandbox['durations'] = [
+      [
+        'title' => '10 minutes or less',
+        'low' => 1,
+        'high' => 659,
+      ],
+      [
+        'title' => '15 minutes',
+        'low' => 660,
+        'high' => 1080,
+      ],
+      [
+        'title' => '20 minutes',
+        'low' => 1081,
+        'high' => 1319,
+      ],
+      [
+        'title' => '30 minutes',
+        'low' => 1320,
+        'high' => 1919,
+      ],
+      [
+        'title' => '45 minutes',
+        'low' => 1920,
+        'high' => 2819,
+      ],
+      [
+        'title' => '60 minutes',
+        'low' => 2820,
+        'high' => 3899,
+      ],
+      [
+        'title' => '90 minutes',
+        'low' => 3900,
+        'high' => 5700,
+      ],
+      [
+        'title' => 'Undefined',
+        'low' => 0,
+        'high' => 0,
+      ],
+    ];
+
+    $sandbox['processed'] = 0;
+    $sandbox['max'] = count($sandbox['durations']);
+  }
+
+  $term_storage = \Drupal::entityTypeManager()
+    ->getStorage('taxonomy_term');
+
+  // Process 5 durations per run.
+  for ($i = 0; $i < 5; $i++) {
+    if (!empty($sandbox['durations'])) {
+      $duration = array_shift($sandbox['durations']);
+      $term = $term_storage->create([
+        'vid'                   => 'gc_duration',
+        'name'                  => $duration['title'],
+        'field_gc_duration_min' => $duration['low'],
+        'field_gc_duration_max' => $duration['high'],
+      ]);
+      $term->save();
+      $sandbox['processed']++;
+    }
+  }
+
+  $sandbox['#finished'] = empty($sandbox['durations']) ? TRUE : $sandbox['processed'] / $sandbox['max'];
+  if ($sandbox['#finished'] === TRUE) {
+    return t('The Duration terms have been created.');
+  }
+}
+
+/**
+ * Helper method to build duration references based on duration field value.
+ */
+function _openy_gc_storage_build_duration_references(&$sandbox) {
+  if (!isset($sandbox['sandbox']['ids'])) {
+    $sandbox['sandbox']['ids'] = \Drupal::entityQuery('node')
+      ->condition('type', 'gc_video')
+      ->sort('nid')
+      ->execute();
+
+    // By default we'll set reference to 'Undefined' duration term.
+    $sandbox['sandbox']['default_duration_term'] = \Drupal::entityQuery('taxonomy_term')
+      ->condition('vid', 'gc_duration')
+      ->condition('field_gc_duration_min', 0)
+      ->condition('field_gc_duration_max', 0)
+      ->range(0, 1)
+      ->execute();
+    if (!empty($sandbox['sandbox']['default_duration_term'])) {
+      $sandbox['sandbox']['default_duration_term'] = reset($sandbox['sandbox']['default_duration_term']);
+    }
+    else {
+      $sandbox['sandbox']['default_duration_term'] = '';
+    }
+
+    $sandbox['sandbox']['max'] = count($sandbox['sandbox']['ids']);
+    $sandbox['sandbox']['processed'] = 0;
+  }
+
+  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+
+  for ($i = 0; $i < 5; $i++) {
+    if (empty($sandbox['sandbox']['ids'])) {
+      break;
+    }
+    $duration_term_tid = $sandbox['sandbox']['default_duration_term'];
+
+    $current_nid = array_shift($sandbox['sandbox']['ids']);
+    $node = $node_storage->load($current_nid);
+    /** @var \Drupal\node\NodeInterface $node */
+    $duration_field = $node->get('field_gc_video_duration');
+    // Let's find id of the duration term that has minimal value less than the
+    // duration of the given video & has maximum value bigger than or equal to
+    // the duration of the given video.
+    if (!$duration_field->isEmpty()) {
+      $duration_value = $duration_field->getString();
+      $duration_term = \Drupal::entityQuery('taxonomy_term')
+        ->condition('vid', 'gc_duration')
+        ->condition('field_gc_duration_min', $duration_value, '<')
+        ->condition('field_gc_duration_max', $duration_value, '>=')
+        ->accessCheck(FALSE)
+        // Let's select only first taxonomy that fits the criteria.
+        ->range(0, 1)
+        ->sort('tid')
+        ->execute();
+      if (!empty($duration_term)) {
+        $duration_term_tid = reset($duration_term);
+      }
+    }
+
+    if (!empty($duration_term_tid)) {
+      $node->set('field_gc_duration_reference', $duration_term_tid)->save();
+    }
+
+    $sandbox['sandbox']['processed']++;
+    $sandbox['results']['processed'] = $sandbox['sandbox']['processed'];
+  }
+
+  $sandbox['#finished'] = empty($sandbox['sandbox']['ids']) ? 1 : $sandbox['sandbox']['processed'] / $sandbox['sandbox']['max'];
+
+  $sandbox['finished'] = $sandbox['#finished'];
+  $sandbox['message'] = t(
+    'Processed @current videos out of total @total',
+    [
+      '@current' => $sandbox['sandbox']['processed'],
+      '@total' => $sandbox['sandbox']['max'],
+    ]
+  );
+}
+
+/**
+ * Add appropriate duration references to all existing 'Virtual Y Video' nodes.
+ */
+function openy_gc_storage_post_update_migrate_node_durations(&$sandbox) {
+  _openy_gc_storage_build_duration_references($sandbox);
+}
