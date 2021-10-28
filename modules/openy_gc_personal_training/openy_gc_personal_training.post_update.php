53e1e67838bacafee0a91dadd68f99e67b53ff82
Vladyslav Solodovnyk <vladyslav.solodovnyk@fivejars.com>
2021-10-28 15:29:37 +0300

    [PRODDEV-342] Prettify post_update implementation, disable admin permission from the Virtual trainer role, etc.

--- modules/openy_gc_personal_training/openy_gc_personal_training.post_update.php
+++ modules/openy_gc_personal_training/openy_gc_personal_training.post_update.php
@@ -43,25 +43,30 @@ function openy_gc_personal_training_post_update_init_owners(&$sandbox) {
   $entity_type = 'personal_training';
   $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
   if (!isset($sandbox['progress'])) {
-    $query = $storage->getQuery($entity_type)->notExists('uid');
-    $sandbox['ids'] = $query->execute();
-    $sandbox['max'] = $query->count()->execute();
+    $sandbox['progress'] = 0;
+    $sandbox['current'] = 0;
+    $sandbox['max'] = $storage->getQuery()
+      ->notExists('uid')
+      ->count()
+      ->execute();
   }
 
-  $ids = array_slice($sandbox['ids'], 0, 20);
+  $ids = $storage->getQuery()
+    ->notExists('uid')
+    ->condition('id', $sandbox['current'], '>')
+    ->range(0, 20)
+    ->sort('id')
+    ->execute();
   $personal_trainings = $storage->loadMultiple($ids);
-  $not_existed = array_diff($ids, array_keys($personal_trainings));
-  if (!empty($not_existed)) {
-    $sandbox['ids'] = array_diff($sandbox['ids'], $not_existed);
-  }
 
   foreach ($personal_trainings as $personal_training) {
     $personal_training->set('uid', $personal_training->instructor_id->target_id);
     $personal_training->save();
-    $sandbox['ids'] = array_diff($sandbox['ids'], [$personal_training->id()]);
+    $sandbox['progress']++;
+    $sandbox['current'] = $personal_training->id();
   }
 
-  $sandbox['#finished'] = (count($sandbox['ids']) === 0) ? TRUE : count($sandbox['ids']) / $sandbox['max'];
+  $sandbox['#finished'] = empty($sandbox['max']) ? 1 : ($sandbox['progress'] / $sandbox['max']);
   if ($sandbox['#finished']) {
     return t('Owners were initialized for @count personal trainings', ['@count' => $sandbox['max']]);
   }
