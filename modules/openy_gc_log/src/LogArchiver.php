d2029f9a4cc6331f2cfa1b4f9887a8a2797ae59d
Anatolii Poliakov <anpolimus@gmail.com>
2021-12-14 19:35:01 +0200

    Update LogArchiver.php

--- modules/openy_gc_log/src/LogArchiver.php
+++ modules/openy_gc_log/src/LogArchiver.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\File\FileSystem;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\Core\Site\Settings;
