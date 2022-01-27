1890119d4bb7e9623377e4abf4bdb3dd83edf76a
Serhii Zherebchuk <serhii@imagexmedia.com>
2022-01-27 18:20:10 +0200

    PRODDEV-509: Added new component for chat for Virtual meeting

---
+++ js/gated-content/src/components/chat-room/live-stream/ChatRoomItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="control-panel text-white">
+    <div class="chat"
+         :class="{'unread': unreadMessagesCount}"
+         @click="toggleShowChatModal"
+    >
+      <SvgIcon icon="question_answer_black_24dp"></SvgIcon>
+      <span v-if="unreadMessagesCount" class="unread-count">
+                  {{ unreadMessagesCount }}
+                </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import SvgIcon from '@/components/SvgIcon.vue';
+
+export default {
+  components: { SvgIcon },
+  computed: {
+    ...mapGetters([
+      'unreadMessagesCount',
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'toggleShowChatModal',
+    ]),
+  },
+};
+</script>
